ea43519938471063138c165c0e498fb1676a78a0
maheshhp <hpmahesh73@gmail.com>
2018-01-31 20:03:21 +0530

    Exercise One - verified exercise

--- ExerciseOne/solution.test.js
+++ ExerciseOne/solution.test.js
@@ -1,9 +1,8 @@
-const solution = require('./solution');
 const axios = require('axios');
 
 describe('Tests for checking the data received from HTTP Get request', () => {
   test('Verify data returned for successful HTTP request', (done) => {
-    axios.get('http://127.0.0.1/')
+    axios.get('http://127.0.0.1:8080/')
       .then((response) => {
         expect(response).toMatch('Hello hapi');
         done();
