dd68a51b5ad426ac340cba0333cbed29594e360a
maheshhp <hpmahesh73@gmail.com>
2018-01-31 20:34:50 +0530

    Exercise Two - verified exercise

--- ExerciseTwo/solution.js
+++ ExerciseTwo/solution.js
@@ -2,13 +2,30 @@ const Hapi = require('hapi');
 
 const server = new Hapi.Server();
 
-let routeHandler = (request) => {
-
-};
+let routeHandler = request => `Hello ${request}`;
 
 server.connection({
   host: 'localhost',
   port: process.argv[2] || 8080,
+});
+
+server.route({
+  config: {
+    cors: {
+      origin: ['*'],
+      additionalHeaders: ['cache-control', 'x-requested-with'],
+    },
+  },
+  path: '/{name}',
+  method: 'GET',
+  handler: (request, reply) => reply(routeHandler(request.params.name)),
+});
+
+server.start((err) => {
+  if (err) {
+    throw err;
+  }
+  console.log(`Server running at: ${server.info.uri}`);
 });
 
 module.exports = routeHandler;
--- ExerciseTwo/solution.test.js
+++ ExerciseTwo/solution.test.js
@@ -3,13 +3,13 @@ const solution = require('./solution');
 
 describe('Tests for checking the data received from HTTP Get request', () => {
   test('Verify data returned for successful HTTP request', (done) => {
-    http.get('http://127.0.0.1:8080/mahesh', (request, response) => {
+    http.get('http://127.0.0.1:8080/mahesh', (response) => {
       response.setEncoding('UTF8');
       let retData = '';
       response.on('data', (data) => {
         retData += data.toString();
       });
-      response.on('end', (end) => {
+      response.on('end', () => {
         expect(retData).toMatch('Hello mahesh');
         done();
       });
@@ -17,5 +17,6 @@ describe('Tests for checking the data received from HTTP Get request', () => {
   });
   test('Verify string returned by the route handler function', (done) => {
     expect(solution('abcd')).toMatch('Hello abcd');
+    done();
   });
 });
