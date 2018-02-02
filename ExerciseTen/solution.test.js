93c9c52543bc0753bce96b5b53d79834a2ae188a
maheshhp <hpmahesh73@gmail.com>
2018-02-02 07:41:07 +0530

    Exercise Ten: Added tests for unsuccessful request types

--- ExerciseTen/solution.test.js
+++ ExerciseTen/solution.test.js
@@ -4,4 +4,10 @@ describe('Tests for successful and unsuccessful login to the server', () => {
   test('Test for successful POST request reply code from the server', (done) => {
     done();
   });
+  test('Test for GET request reply code from the server', (done) => {
+    done();
+  });
+  test('Test for unsuccessful POST request reply code from the server', (done) => {
+    done();
+  });
 });
