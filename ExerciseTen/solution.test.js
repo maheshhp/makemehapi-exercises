84b98733de61366869708fd593500b37e00407d9
maheshhp <hpmahesh73@gmail.com>
2018-02-02 07:35:34 +0530

    Exercise Ten: Added test for reply code on successful POST reqquest and boilerplate code

---
+++ ExerciseTen/solution.test.js
@@ -0,0 +1,7 @@
+const server = require('./solution');
+
+describe('Tests for successful and unsuccessful login to the server', () => {
+  test('Test for successful POST request reply code from the server', (done) => {
+    done();
+  });
+});
