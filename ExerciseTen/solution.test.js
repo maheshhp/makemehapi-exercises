75251f636b3853c38048b139238d0c965307389a
maheshhp <hpmahesh73@gmail.com>
2018-02-02 07:45:44 +0530

    Exercise Ten: Added tests for valid and invalid user login requests

--- ExerciseTen/solution.test.js
+++ ExerciseTen/solution.test.js
@@ -10,4 +10,13 @@ describe('Tests for successful and unsuccessful login to the server', () => {
   test('Test for unsuccessful POST request reply code from the server', (done) => {
     done();
   });
+  test('Test for guest login reply message from the server', (done) => {
+    done();
+  });
+  test('Test for valid user login reply message from the server', (done) => {
+    done();
+  });
+  test('Test for invalid user login reply message from the server', (done) => {
+    done();
+  });
 });
